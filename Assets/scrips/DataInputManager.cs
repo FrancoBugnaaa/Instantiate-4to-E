f6fe8c90b833dadd104aa9108dbc0166afa1bccd
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-05-10 15:39:31 -0300

    Completed Input Field

---
+++ Assets/scrips/DataInputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+    public Text txtUsername;
+
+    public void ShowGreetings()
+    {
+        Debug.Log("Hola " + txtUsername.text);
+    }
+
+    public void ShowDouble()
+    {
+        if(txtUsername.text != "")
+        {
+            Debug.Log(int.Parse(txtUsername.text) * 2);
+        }
+        else
+        {
+            Debug.Log("El campo no puede estar vacio");
+        }
+    }
+   
+}
